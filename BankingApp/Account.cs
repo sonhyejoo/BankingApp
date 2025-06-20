9d5cede01a4eb693d7cf901efba82e3463c77407
Seong Woo Cho <seongwoo.cho@gmail.com>
2025-06-20 14:16:52 -0400

    Added Creation, Deposit, Withdraw, Balance, Transfer menus

--- BankingApp/Account.cs
+++ BankingApp/Account.cs
@@ -5,28 +5,26 @@ namespace BankingApp;
 
 public class Account
 {
-    private static int s_numberSeed = 1234567890;
-    public string AccountNumber { get; set; }
-    public string Name { get; set; }
+    public string AccountNumber { get; }
+    public string Name { get; }
     public decimal Balance { get; protected set; }
 
     public Account(string name, decimal balance)
     {
         Name = name;
         Balance = balance;
-        AccountNumber = s_numberSeed.ToString();
-        s_numberSeed++;
-    }
+        AccountNumber = Guid.NewGuid().ToString();
+    }   
 
     public void Deposit(decimal depositAmount)
     {
         Balance += depositAmount;
         return;
     }
-
+    
     public void Withdraw(decimal withdrawAmount)
     {
-        Balance += withdrawAmount;
+        Balance -= withdrawAmount;
         return;
     }
 
--- BankingApp/Utilities.cs
+++ BankingApp/Utilities.cs
@@ -1,13 +1,12 @@
-using System;
-using System.ComponentModel;
 
 namespace BankingApp;
 
 public class Utilities
 {
-    private static List<Account> s_accounts = new();
+    private static List<Account> _accounts = new();
     public static void ShowMainMenu()
     {
+        // Console.ResetColor();
         Console.Clear();
         Console.WriteLine("Select an option: ");
         Console.WriteLine("1. Create account");
@@ -42,49 +41,211 @@ public class Utilities
                 break;
             default:
                 Console.WriteLine("Please enter a valid selection. Please try again.");
+                ShowMainMenu();
                 break;
         }
     }
-
-
-
-
+    
     private static void ShowAccountCreationMenu()
     {
+        Console.Clear();
         Console.WriteLine("Account creation: \n");
-        Console.WriteLine("Please enter your name: ");
-        string? userName = Console.ReadLine();
-        Console.WriteLine("Please enter initial deposit amount: ");
-        if (!decimal.TryParse(Console.ReadLine(), out decimal userBalance))
+        bool emptyName = true;
+        string? userName = null;
+        while (emptyName)
         {
-            Console.WriteLine("Invalid initial deposit amount. Please enter a valid decimal for initial deposit.");
-
+            Console.WriteLine("Please enter account holder's name: ");
+            userName = Console.ReadLine();
+            if (string.IsNullOrEmpty(userName))
+            {
+                Console.WriteLine("Name cannot be empty.");
+            }
+            else
+            {
+                emptyName = false;
+            }
         }
 
-        Account newAccount = new(userName, userInitialBalance);
+        bool validBalance = false;
+        decimal userBalance = 0;
+        while (!validBalance)
+        {
+            Console.WriteLine("Please enter initial deposit amount: ");
+            if (!decimal.TryParse(Console.ReadLine(), out userBalance))
+            {
+                Console.WriteLine("Initial deposit must be a valid decimal value.");
+                continue;
+            }
 
+            if (userBalance <= 0)
+            {
+                Console.WriteLine("Initial deposit amount must be greater than zero.");
+            }
+            else
+            {
+                validBalance = true;
+            }
+        }
+
+        Account newAccount = new(userName, userBalance);
+        _accounts.Add(newAccount);
+        Console.WriteLine("New bank account created with the following details: ");
+        Console.WriteLine($"Name of account holder: {newAccount.Name}");
+        Console.WriteLine($"Account's initial balance: ${newAccount.Balance}");
+        Console.WriteLine($"Account number used for access: {newAccount.AccountNumber}");
+        Console.WriteLine("Press any button to return to main menu...");
+
+        Console.ReadKey();
+        ShowMainMenu();
     }
 
     private static void ShowDepositMenu()
     {
-        Console.WriteLine("Deposit Funds: ");
-        Console.WriteLine("Please enter account id: ");
-        string? userId = Console.ReadLine();
-        
+        Console.WriteLine("Deposit Funds: \n");
+        Account? foundAccount = FindAccountMenuPrompt();
+
+        bool validDeposit = false;
+        while (!validDeposit)
+        {
+            Console.WriteLine("Please enter deposit amount: ");
+            if (!decimal.TryParse(Console.ReadLine(), out decimal depositAmount))
+            {
+                Console.WriteLine("Deposit amount must be a valid decimal value.");
+                continue;
+            }
+
+            if (depositAmount <= 0)
+            {
+                Console.WriteLine("Deposit amount must be greater than zero.");
+            }
+            else
+            {
+                foundAccount.Deposit(depositAmount);
+                Console.WriteLine($"Deposit successful. New balance is: ${foundAccount.Balance}");
+                validDeposit = true;
+            }
+        }
+        Console.WriteLine("Press any key to return to main menu");
+        Console.ReadKey();
+        ShowMainMenu();
     }
+
 
     private static void ShowWithdrawMenu()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Withdraw Menu: \n");
+        Account? foundAccount = FindAccountMenuPrompt();
+        
+        bool validWithdraw = false;
+        while (!validWithdraw)
+        {
+            Console.WriteLine("Please enter withdraw amount: ");
+            if (!decimal.TryParse(Console.ReadLine(), out decimal withdrawAmount))
+            {
+                Console.WriteLine("Withdrawal amount must be a valid decimal value.");
+                continue;
+            }
+
+            if (withdrawAmount <= 0)
+            {
+                Console.WriteLine("Withdrawal amount must be greater than zero.");
+            }
+            else if (withdrawAmount > foundAccount.Balance)
+            {
+                Console.WriteLine($"Withdraw amount cannot exceed your current balance of ${foundAccount.Balance}.");
+            }
+            else
+            {
+                foundAccount.Withdraw(withdrawAmount);
+                Console.WriteLine(
+                    $"${withdrawAmount} successfully withdrawn. New balance is: ${foundAccount.Balance}");
+                validWithdraw = true;
+            }
+        }
+        Console.WriteLine("Press any key to return to main menu");
+        Console.ReadKey();
+        ShowMainMenu();
     }
 
     private static void ShowBalanceMenu()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Balance Menu: \n");
+        Account? foundAccount = FindAccountMenuPrompt();
+        Console.WriteLine($"Current account balance is: {foundAccount.Balance} \n");
+        
+        Console.WriteLine("Press any key to return to main menu");
+        Console.ReadKey();
+        ShowMainMenu();
     }
 
     private static void ShowTransferMenu()
     {
-        throw new NotImplementedException();
+        Console.WriteLine("Transfer Funds Menu: \n");
+        Account? sendingAccount = FindAccountMenuPrompt();
+
+        Account? receiveAccount = FindAccountMenuPrompt();
+        bool validTransfer = false;
+        while (!validTransfer)
+        {
+            Console.WriteLine("Please enter transfer amount: ");
+            if (!decimal.TryParse(Console.ReadLine(), out decimal transferAmount))
+            {
+                Console.WriteLine("Transfer amount must be a valid decimal value.");
+                continue;
+            }
+
+            if (transferAmount <= 0)
+            {
+                Console.WriteLine("Transfer amount must be greater than zero.");
+            }
+            else if (transferAmount > sendingAccount.Balance)
+            {
+                Console.WriteLine($"Transfer amount cannot exceed sender's balance of ${sendingAccount.Balance}.");
+            }
+            else
+            {
+                sendingAccount.Withdraw(transferAmount);
+                receiveAccount.Deposit(transferAmount);
+                Console.WriteLine(
+                    $"\n ${transferAmount} successfully transferred.");
+                Console.WriteLine($"Sender's new balance is: ${sendingAccount.Balance}");
+                Console.WriteLine($"Recipients's balance is: ${receiveAccount.Balance} \n");
+                validTransfer = true;
+            }
+        }
+
+        Console.WriteLine("Press any key to return to main menu");
+        Console.ReadKey();
+        ShowMainMenu();
     }
+    private static Account? FindAccountWithNumberOrNull(string? accountNumber)
+    {
+        var foundAccount = _accounts.FirstOrDefault(a => a.AccountNumber == accountNumber);
+        return foundAccount;
+    }
+    private static Account? FindAccountMenuPrompt()
+    {
+        Account? foundAccount = null;
+        bool accountWasFound = false;
+        while (!accountWasFound)
+        {
+            Console.WriteLine("Please enter account number: ");
+
+            string? inputNumber = Console.ReadLine();
+            var findingAccount = FindAccountWithNumberOrNull(inputNumber);
+            if (findingAccount is null)
+            {
+                Console.WriteLine("No account found with that account number.");
+            }
+            else
+            {
+                Console.WriteLine("Account found.");
+                foundAccount = findingAccount;
+                accountWasFound = true;
+            }
+        }
+
+        return foundAccount;
+    }
+
 }
